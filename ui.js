d4fea72de71b6fa6f3d3535eeeffa6e7599972bb
ZorakiMühendis <46242971+armes-7@users.noreply.github.com>
2021-01-21 10:57:23 +0300

    Add files via upload

---
+++ ui.js
@@ -0,0 +1,15 @@
+function UI(){
+    this.outputImage = document.getElementById("outputImage");
+    this.outputLanguage = document.getElementById("outputLanguage");
+    this.outputWord = document.getElementById("language");
+}
+
+UI.prototype.changeUI = function(){
+// Arayüz değiştirilecek
+  this.outputImage.src = `images/${this.languageList.value}.png`;
+  
+
+}
+UI.prototype.displayTranslate = function(word){
+    this.outputWord.textContent = word;
+}
